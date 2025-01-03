24f8d87a88a06a11a5e2790fab117200a192d9a1
S0NA1 <129035989+S0NA1@users.noreply.github.com>
2025-01-04 04:42:42 +1000

    Delete DJANGO directory

--- DJANGO/my_site/admins/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import AdminsMain, Role, Permission, ActivityLog, RolePermission
-
-admin.site.register(AdminsMain)
-admin.site.register(Role)
-admin.site.register(Permission)
-admin.site.register(RolePermission)
-admin.site.register(ActivityLog)
